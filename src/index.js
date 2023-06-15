9a7659b37d0df0cc577d0517e696cfe724741f0c
Naomi Elvira <naomiet97@gmail.com>
2023-06-15 22:07:10 +0200

    create sliders for landing page

--- src/index.js
+++ src/index.js
@@ -9,4 +9,12 @@ LandingContent();
 Footer();
  */
 
-
+$(document).ready(function () {
+  $(".slick-carousel").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+  });
+});
