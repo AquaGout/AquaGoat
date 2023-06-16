f39a6f3fd42a5bdb64c7f7c2896d93b590ba41a7
Naomi Elvira <naomiet97@gmail.com>
2023-06-16 15:08:05 +0200

    change the graphics for the landing page

--- src/components/LandingContent/LandingContent.js
+++ src/components/LandingContent/LandingContent.js
@@ -1,12 +1,10 @@
-
 $(document).ready(function () {
   $(".slick-carousel").slick({
-    dots: true,
-    infinite: true,
+    infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    speed: 500,
-    fade: true,
-    cssEase: "linear",
+    dots: true,
+    arrows: true,
+    autoplay: false,
   });
 });
