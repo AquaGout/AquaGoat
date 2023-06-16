b2d44538bea620ab9340158a30f3ea87e1a5f76e
Naomi Elvira <naomiet97@gmail.com>
2023-06-16 16:54:02 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+  $(".slick-carousel").slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    autoplay: false,
+  });
+});
